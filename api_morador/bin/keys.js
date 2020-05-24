17aad4a0de6a02ca0cf20a7c654da13825fd6ea6
leonardobussi <leonardobezerrabussy@gmail.com>
2020-05-24 17:22:30 -0300

    ta ae essa merda

--- api_morador/bin/keys.js
+++ api_morador/bin/keys.js
@@ -5,7 +5,7 @@ module.exports = {
         port: process.env.port || 3000,
     },
     database: {
-        connection: 'mongodb'
+        connection: 'mongodb+srv://portaria:portaria@cluster0-e2g2g.mongodb.net/teste?retryWrites=true&w=majority'
     },
     auth: {
         secret: 'c1c2c3c4c5'
