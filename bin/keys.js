7ef923ec06a7653ff7138aa2ad128b47904f7287
leonardobussi <leonardobezerrabussy@gmail.com>
2020-06-14 17:03:30 -0300

    faca o deploy

--- bin/keys.js
+++ bin/keys.js
@@ -2,7 +2,7 @@
 
 module.exports = {
     server: {
-        port: process.env.port || 3000,
+        port: process.env.PORT || 3000,
     },
     database: {
         connection: 'mongodb+srv://portaria:portaria@cluster0-e2g2g.mongodb.net/teste?retryWrites=true&w=majority' 
